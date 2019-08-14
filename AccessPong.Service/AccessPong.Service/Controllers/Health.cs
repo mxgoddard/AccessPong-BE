5bdeb7499f1256f7328370842ab877dd0be417c7
Stefano Artoni <stefano@stefano.home>
2019-08-14 20:53:54 +0100

    First setup of the service with a health message

---
+++ AccessPong.Service/AccessPong.Service/Controllers/Health.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccessPong.Service.Controllers
+{
+    [Route("api/health")]
+    public class Health : ControllerBase
+    {    
+        // Return a positive health message of the service
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("The Service is healthy and working properly.");
+        }            
+    }
+}
