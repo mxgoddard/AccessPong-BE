c6d402b1df03ff9c6d3ebf92ebde4e14e9c41c51
Max Goddard <Max.Goddard@accesspay.com>
2019-08-15 12:00:18 +0100

    Created Basic Endpoints

---
+++ AccessPong.Service/AccessPong.Service/Controllers/Endpoints.cs
@@ -0,0 +1,31 @@
+﻿using AccessPong.Service.Event;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessPong.Service.Controllers
+{
+    [Route("api")]
+    public class Endpoints : ControllerBase
+    {
+        // Next match endpoint
+        [HttpGet("next")]
+        public IActionResult GetNextMatch()
+        {
+            return Ok($"{DateTime.UtcNow}: Next match endpoint hit");
+        }
+
+        // Next match endpoint
+        [HttpGet("league")]
+        public IActionResult GetLeague()
+        {
+            return Ok($"{DateTime.UtcNow}: Get league endpoint hit");
+        }
+
+        // Next match endpoint
+        [HttpGet("fixtures")]
+        public IActionResult GetFixtures()
+        {
+            return Ok($"{DateTime.UtcNow}: Get fixtures endpoint hit");
+        }
+    }
+}
