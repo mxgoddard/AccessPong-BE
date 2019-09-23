244c82ca56b61f54734e2bdf5773de5626d4ad52
Max Goddard <Max.Goddard@accesspay.com>
2019-09-23 16:41:38 +0100

    Split out the controllers

---
+++ AccessPong/Controllers/FixturesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AccessPong.Events.Helper;
+using AccessPong.Events.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AccessPong.Controllers
+{
+    [Route("api/fixtures")]
+    [ApiController]
+    public class FixturesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        private readonly IHelper _helper;
+
+        public FixturesController(ILogger<ValuesController> logger, IHelper helper)
+        {
+            _logger = logger;
+            _helper = helper;
+        }
+
+        // GET api/fixtures
+        [HttpGet()]
+        public IActionResult GetFixtures()
+        {
+            var fixturesJson = _helper.GetFixtures();
+
+            return Content(fixturesJson, "application/json");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTest(int id)
+        {
+            Console.WriteLine(id);
+            var fixturesJson = _helper.GetFixtures();
+
+            return Content(fixturesJson, "application/json");
+        }
+
+        // GET api/fixtures/generate
+        [HttpGet("generate")]
+        public IActionResult GenerateFixturesEndpoint()
+        {
+            bool IS_ADMIN = true;
+
+            if (!IS_ADMIN)
+            {
+                return Unauthorized($"{DateTime.UtcNow}: Fixtures failed to generate.");
+            }
+
+            bool success = _helper.GenerateFixtures();
+
+            if (!success)
+            {
+                return NotFound($"{DateTime.UtcNow}: Fixtures failed to generate.");
+            }
+
+            return Ok($"{DateTime.UtcNow}: Fixtures successfully generated.");
+        }
+
+        // GET api/fixtures/next
+        [HttpGet("next")]
+        public IActionResult GetNextFixture()
+        {
+            var fixtureJson = _helper.GetNextGame();
+
+            return Content(fixtureJson, "application/json");
+        }
+
+        // POST api/fixtures/update
+        [HttpPost("update")]
+        public IActionResult UpdateFixture([FromBody]FixtureUpdate data)
+        {
+            var fixtureJson = _helper.UpdateFixture(data.FixtureId, data.WinnerId);
+
+            return Content(fixtureJson, "application/json");
+        }
+    }
+}
